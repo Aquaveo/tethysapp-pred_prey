20c382fb480a1c8c53b3684c4b570b1ec3fd5b65
Nathan Swain <nswain@aquaveo.com>
2023-11-11 18:21:20 +0000

    Stub out controls

--- tethysapp/pred_prey/controllers.py
+++ tethysapp/pred_prey/controllers.py
@@ -7,5 +7,12 @@ def home(request):
     """
     Controller for the app home page.
     """
-    context = {}
+    context = {
+        'initial_x0': 100,
+        'initial_y0': 100,
+        'initial_alpha': 0.005,
+        'initial_beta': 0.00005,
+        'initial_delta': 0.00007,
+        'initial_gamma': 0.002,
+    }
     return render(request, 'pred_prey/home.html', context)
